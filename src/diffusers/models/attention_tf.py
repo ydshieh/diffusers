cdbeae813b0d981715c30a10b28c157cd922f0b0
ydshieh <ydshieh@users.noreply.github.com>
2022-09-18 22:00:27 +0200

    Add TFCrossAttention

--- src/diffusers/models/attention_tf.py
+++ src/diffusers/models/attention_tf.py
@@ -313,7 +313,115 @@ class TFAttentionBlock(tf.keras.layers.Layer):
         # res connect and rescale
         hidden_states = (hidden_states + residual) / self.rescale_output_factor
         return hidden_states
-        
+
+
+class TFCrossAttention(tf.keras.layers.Layer):
+    r"""
+    A cross attention layer.
+
+    Parameters:
+        query_dim (:obj:`int`): The number of channels in the query.
+        context_dim (:obj:`int`, *optional*):
+            The number of channels in the context. If not given, defaults to `query_dim`.
+        heads (:obj:`int`,  *optional*, defaults to 8): The number of heads to use for multi-head attention.
+        dim_head (:obj:`int`,  *optional*, defaults to 64): The number of channels in each head.
+        dropout (:obj:`float`, *optional*, defaults to 0.0): The dropout probability to use.
+    """
+
+    def __init__(
+        self, query_dim: int, context_dim: Optional[int] = None, heads: int = 8, dim_head: int = 64, dropout: int = 0.0
+    ):
+        super().__init__()
+        inner_dim = dim_head * heads
+        context_dim = context_dim if context_dim is not None else query_dim
+
+        self.scale = dim_head**-0.5
+        self.heads = heads
+        # for slice_size > 0 the attention score computation
+        # is split across the batch axis to save memory
+        # You can set slice_size with `set_attention_slice`
+        self._slice_size = None
+
+        self.to_q = tf.keras.layers.Dense(units=inner_dim, use_bias=False, name="to_q")
+        self.to_k = tf.keras.layers.Dense(units=inner_dim, use_bias=False, name="to_k")
+        self.to_v = tf.keras.layers.Dense(units=inner_dim, use_bias=False, name="to_v")
+
+        output_layer = tf.keras.layers.Dense(query_dim, name="to_out_._0")
+        dropout_layer = tf.keras.layers.Dropout(dropout)
+        self.to_out = [output_layer, dropout_layer]
+
+    def reshape_heads_to_batch_dim(self, tensor):
+        batch_size, seq_len, dim = tensor.shape
+        head_size = self.heads
+        tensor = tf.reshape(tensor, shape=(batch_size, seq_len, head_size, dim // head_size))
+        tensor = tf.reshape(tf.transpose(tensor, perm=(0, 2, 1, 3)), shape=(batch_size * head_size, seq_len, dim // head_size))
+        return tensor
+
+    def reshape_batch_dim_to_heads(self, tensor):
+        batch_size, seq_len, dim = tensor.shape
+        head_size = self.heads
+        tensor = tf.reshape(tensor, shape=(batch_size // head_size, head_size, seq_len, dim))
+        tensor = tf.reshape(tf.transpose(tensor, perm=(0, 2, 1, 3)), shape=(batch_size // head_size, seq_len, dim * head_size))
+        return tensor
+
+    def call(self, hidden_states, context=None, mask=None):
+        # TODO: Remove once ready
+        if context is None:
+            return
+
+        batch_size, sequence_length, dim = hidden_states.shape
+
+        query = self.to_q(hidden_states)
+        context = context if context is not None else hidden_states
+        key = self.to_k(context)
+        value = self.to_v(context)
+
+        query = self.reshape_heads_to_batch_dim(query)
+        key = self.reshape_heads_to_batch_dim(key)
+        value = self.reshape_heads_to_batch_dim(value)
+
+        # TODO(PVP) - mask is currently never used. Remember to re-implement when used
+
+        # attention, what we cannot get enough of
+
+        # if self._slice_size is None or query.shape[0] // self._slice_size == 1:
+        hidden_states = self._attention(query, key, value)
+        # else:
+        #     hidden_states = self._sliced_attention(query, key, value, sequence_length, dim)
+
+        for layer in self.to_out:
+            hidden_states = layer(hidden_states)
+
+        return hidden_states
+
+    def _attention(self, query, key, value):
+        attention_scores = tf.matmul(query, tf.transpose(key, perm=(0, 2, 1))) * self.scale
+        attention_probs = tf.math.softmax(attention_scores, axis=-1)
+        # compute attention output
+        hidden_states = tf.matmul(attention_probs, value)
+        # reshape hidden_states
+        hidden_states = self.reshape_batch_dim_to_heads(hidden_states)
+        return hidden_states
+
+    # def _sliced_attention(self, query, key, value, sequence_length, dim):
+    #     batch_size_attention = query.shape[0]
+    #     hidden_states = torch.zeros(
+    #         (batch_size_attention, sequence_length, dim // self.heads), device=query.device, dtype=query.dtype
+    #     )
+    #     slice_size = self._slice_size if self._slice_size is not None else hidden_states.shape[0]
+    #     for i in range(hidden_states.shape[0] // slice_size):
+    #         start_idx = i * slice_size
+    #         end_idx = (i + 1) * slice_size
+    #         attn_slice = torch.matmul(query[start_idx:end_idx], key[start_idx:end_idx].transpose(1, 2)) * self.scale
+    #         attn_slice = attn_slice.softmax(dim=-1)
+    #         attn_slice = torch.matmul(attn_slice, value[start_idx:end_idx])
+    #
+    #         hidden_states[start_idx:end_idx] = attn_slice
+    #
+    #     # reshape hidden_states
+    #     hidden_states = self.reshape_batch_dim_to_heads(hidden_states)
+    #     return hidden_states
+
         
 class TFFeedForward(tf.keras.layers.Layer):
     r"""
--- tests/more_tests/models/test_attentions_tf.py
+++ tests/more_tests/models/test_attentions_tf.py
@@ -3,8 +3,8 @@ import torch
 import tensorflow as tf
 import unittest
 
-from diffusers.models.attention_tf import TFGEGLU, TFFeedForward, TFAttentionBlock
-from diffusers.models.attention import GEGLU, FeedForward, AttentionBlock
+from diffusers.models.attention_tf import TFGEGLU, TFFeedForward, TFAttentionBlock, TFCrossAttention
+from diffusers.models.attention import GEGLU, FeedForward, AttentionBlock, CrossAttention
 
 from transformers import load_pytorch_model_in_tf2_model
 
@@ -98,6 +98,39 @@ class TFAttentionBlockTest(unittest.TestCase):
         tf_output = tf_layer(tf_sample)
         # (N, H, W, C) -> (N, C, H, W)
         tf_output = tf.transpose(tf_output, perm=(0, 3, 1, 2))
+
+        max_diff = np.amax(np.abs(pt_output.numpy() - tf_output.numpy()))
+        assert max_diff < 1e-6
+
+
+class TFCrossAttentionTest(unittest.TestCase):
+
+    def test_pt_tf_default(self):
+        N, T_query, T_context, query_dim, context_dim, heads, dim_head = (1, 5, 3, 8, 4, 2, 8)
+
+        sample = np.random.default_rng().standard_normal(size=(N, T_query, query_dim), dtype=np.float32)
+        context = np.random.default_rng().standard_normal(size=(N, T_context, context_dim), dtype=np.float32)
+
+        pt_sample = torch.tensor(sample, dtype=torch.float32)
+        tf_sample = tf.constant(sample)
+
+        pt_context = torch.tensor(context, dtype=torch.float32)
+        tf_context = tf.constant(context)
+
+        pt_layer = CrossAttention(query_dim=query_dim, context_dim=context_dim, heads=heads, dim_head=dim_head)
+        tf_layer = TFCrossAttention(query_dim=query_dim, context_dim=context_dim, heads=heads, dim_head=dim_head)
+
+        # init. TF weights
+        _ = tf_layer(tf_sample, context=tf_context)
+        # Load PT weights
+        tf_layer.base_model_prefix = ""
+        tf_layer._keys_to_ignore_on_load_missing = []
+        tf_layer._keys_to_ignore_on_load_unexpected = []
+        load_pytorch_model_in_tf2_model(tf_layer, pt_layer, tf_inputs=tf_sample, allow_missing_keys=False)
+
+        with torch.no_grad():
+            pt_output = pt_layer(pt_sample, context=pt_context)
+        tf_output = tf_layer(tf_sample, context=tf_context)
 
         max_diff = np.amax(np.abs(pt_output.numpy() - tf_output.numpy()))
         assert max_diff < 1e-6
