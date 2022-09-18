6141551a5720a8f162017355357feee0b68f12d7
ydshieh <ydshieh@users.noreply.github.com>
2022-09-18 14:39:28 +0200

    Add TFUpBlock2D

--- src/diffusers/models/unet_blocks_tf.py
+++ src/diffusers/models/unet_blocks_tf.py
@@ -14,7 +14,7 @@
 
 import tensorflow as tf
 
-from .resnet_tf import TFDownsample2D, TFResnetBlock2D
+from .resnet_tf import TFUpsample2D, TFDownsample2D, TFResnetBlock2D
 
 
 class TFDownBlock2D(tf.keras.layers.Layer):
@@ -75,3 +75,72 @@ class TFDownBlock2D(tf.keras.layers.Layer):
             output_states += (hidden_states,)
 
         return hidden_states, output_states
+
+
+class TFUpBlock2D(tf.keras.layers.Layer):
+
+    def __init__(
+        self,
+        in_channels: int,
+        prev_output_channel: int,
+        out_channels: int,
+        temb_channels: int,
+        dropout: float = 0.0,
+        num_layers: int = 1,
+        resnet_eps: float = 1e-6,
+        resnet_time_scale_shift: str = "default",
+        resnet_act_fn: str = "swish",
+        resnet_groups: int = 32,
+        resnet_pre_norm: bool = True,
+        output_scale_factor=1.0,
+        add_upsample=True,
+        **kwargs,
+    ):
+        super().__init__(**kwargs)
+
+        self.resnets = []
+        for i in range(num_layers):
+            res_skip_channels = in_channels if (i == num_layers - 1) else out_channels
+            resnet_in_channels = prev_output_channel if i == 0 else out_channels
+
+            self.resnets.append(
+                TFResnetBlock2D(
+                    in_channels=resnet_in_channels + res_skip_channels,
+                    out_channels=out_channels,
+                    temb_channels=temb_channels,
+                    eps=resnet_eps,
+                    groups=resnet_groups,
+                    dropout=dropout,
+                    time_embedding_norm=resnet_time_scale_shift,
+                    non_linearity=resnet_act_fn,
+                    output_scale_factor=output_scale_factor,
+                    pre_norm=resnet_pre_norm,
+                    name=f"resnets_._{i}",
+                )
+            )
+
+        if add_upsample:
+            self.upsamplers = [TFUpsample2D(channels=out_channels, use_conv=True, out_channels=out_channels, name=f"upsamplers_._{0}")]
+        else:
+            self.upsamplers = None
+
+    # TODO: Avoid `None` for `res_hidden_states_tuple`.
+    def call(self, hidden_states, res_hidden_states_tuple=None, temb=None):
+
+        # TODO: Remove this block once we can have dict as inputs
+        if res_hidden_states_tuple is None:
+            return
+
+        for resnet in self.resnets:
+            # pop res hidden states
+            res_hidden_states = res_hidden_states_tuple[-1]
+            res_hidden_states_tuple = res_hidden_states_tuple[:-1]
+            hidden_states = tf.concat([hidden_states, res_hidden_states], axis=-1)
+
+            hidden_states = resnet(hidden_states, temb)
+
+        if self.upsamplers is not None:
+            for upsampler in self.upsamplers:
+                hidden_states = upsampler(hidden_states)
+
+        return hidden_states
--- tests/more_tests/models/test_unet_blocks_tf.py
+++ tests/more_tests/models/test_unet_blocks_tf.py
@@ -3,8 +3,8 @@ import torch
 import tensorflow as tf
 import unittest
 
-from diffusers.models.unet_blocks_tf import TFDownBlock2D
-from diffusers.models.unet_blocks import DownBlock2D
+from diffusers.models.unet_blocks_tf import TFUpBlock2D, TFDownBlock2D
+from diffusers.models.unet_blocks import UpBlock2D, DownBlock2D
 
 from transformers import load_pytorch_model_in_tf2_model
 
@@ -50,3 +50,57 @@ class TFDownBlock2DTest(unittest.TestCase):
                 _tf_o = tf.transpose(_tf_o, perm=(0, 3, 1, 2))
                 max_diff = np.amax(np.abs(np.array(_pt_o) - np.array(_tf_o)))
                 assert max_diff < 5e-6
+
+
+class TFUpBlock2DTest(unittest.TestCase):
+
+    def test_pt_tf_default(self):
+        N, H, W, C = (1, 16, 16, 6)
+        prev_out_C = 6
+        out_C = 3
+        temb_channels = 16
+
+        sample = np.random.default_rng().standard_normal(size=(N, C, H, W), dtype=np.float32)
+        temb = np.random.default_rng().standard_normal(size=(N, temb_channels), dtype=np.float32)
+
+        # TODO: Try to understand what should be the correct numbers of channels.
+        h_0 = np.random.default_rng().standard_normal(size=(N, C, H, W), dtype=np.float32)
+        h_1 = np.random.default_rng().standard_normal(size=(N, out_C, H, W), dtype=np.float32)
+
+        pt_sample = torch.tensor(sample)
+        # (N, C, H, W) -> (N, H, W, C) for TF
+        tf_sample = tf.transpose(tf.constant(sample), perm=(0, 2, 3, 1))
+
+        pt_h_0 = torch.tensor(h_0)
+        # (N, C, H, W) -> (N, H, W, C) for TF
+        tf_h_0 = tf.transpose(tf.constant(h_0), perm=(0, 2, 3, 1))
+
+        pt_h_1 = torch.tensor(h_1)
+        # (N, C, H, W) -> (N, H, W, C) for TF
+        tf_h_1 = tf.transpose(tf.constant(h_1), perm=(0, 2, 3, 1))
+
+        pt_res_hidden_states_tuple = (pt_h_0, pt_h_1)
+        tf_res_hidden_states_tuple = (tf_h_0, tf_h_1)
+
+        pt_temb = torch.tensor(temb, dtype=torch.float32)
+        tf_temb = tf.constant(temb)
+
+        pt_layer = UpBlock2D(in_channels=C, prev_output_channel=prev_out_C, out_channels=out_C, temb_channels=temb_channels, num_layers=2, resnet_groups=out_C)
+        tf_layer = TFUpBlock2D(in_channels=C, prev_output_channel=prev_out_C, out_channels=out_C, temb_channels=temb_channels, num_layers=2, resnet_groups=out_C)
+
+        # init. TF weights
+        _ = tf_layer(tf_sample, res_hidden_states_tuple=tf_res_hidden_states_tuple, temb=temb)
+        # Load PT weights
+        tf_layer.base_model_prefix = ""
+        tf_layer._keys_to_ignore_on_load_missing = []
+        tf_layer._keys_to_ignore_on_load_unexpected = []
+        load_pytorch_model_in_tf2_model(tf_layer, pt_layer, tf_inputs=tf_sample, allow_missing_keys=False)
+
+        with torch.no_grad():
+            pt_output = pt_layer(pt_sample, res_hidden_states_tuple=pt_res_hidden_states_tuple, temb=pt_temb)
+        tf_output = tf_layer(tf_sample, res_hidden_states_tuple=tf_res_hidden_states_tuple, temb=tf_temb)
+        # (N, H, W, C) -> (N, C, H, W)
+        tf_output = tf.transpose(tf_output, perm=(0, 3, 1, 2))
+
+        max_diff = np.amax(np.abs(pt_output.numpy() - tf_output.numpy()))
+        assert max_diff < 1e-6
